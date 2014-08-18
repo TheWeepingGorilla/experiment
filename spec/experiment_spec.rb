8f21730f2b46a620460d2b3a8392e292ee7a52e2
Sean Morrison <student@epicodus.com>
2014-08-17 21:05:24 -0700

    Add association tests to spec.

--- spec/experiment_spec.rb
+++ spec/experiment_spec.rb
@@ -5,4 +5,12 @@ describe 'Experiment' do
     new_experiment = Experiment.create({:description => "Shock Test", :location => "Lab 1"})
     expect(Experiment.no_start[0]).to eq new_experiment
   end
+  it "is associated with many scientists" do
+    new_scientist = Scientist.create({:name => "Dr. Bronner"})
+    new_scientist2 = Scientist.create({:name => "Dr. Spock"})
+    experiment1 = Experiment.create({:description => "Shock Test", :location => "Lab 1"})
+    experiment1.scientists << new_scientist
+    experiment1.scientists << new_scientist2
+    expect(experiment1.scientists).to eq [new_scientist,new_scientist2]
+  end
 end
--- spec/scientist_spec.rb
+++ spec/scientist_spec.rb
@@ -5,4 +5,13 @@ describe 'Scientist' do
     new_scientist = Scientist.create({:name => "Ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"})
     expect(Scientist.all).to eq []
   end
+  it "is associated with many experiments" do
+    new_scientist = Scientist.create({:name => "Dr. Bronner"})
+    experiment1 = Experiment.create({:description => "Shock Test", :location => "Lab 1"})
+    experiment2 = Experiment.create({:description => "Road Test", :location => "Parking Lot"})
+    new_scientist.experiments << experiment1
+    new_scientist.experiments << experiment2
+    expect(new_scientist.experiments).to eq [experiment1,experiment2]
+  end
 end
+
