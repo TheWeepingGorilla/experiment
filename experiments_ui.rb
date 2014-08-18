f5fa576864372b75b324dfeb5734c2b9df177c1a
Sean Morrison <student@epicodus.com>
2014-08-17 20:25:44 -0700

    Edit List and Delete experiments.

--- experiments_ui.rb
+++ experiments_ui.rb
@@ -63,5 +63,68 @@ def add_experiment
   gets
 end
 
+def edit_experiment
+  list_experiments
+  puts "Please enter id number of experiment to edit."
+  id_to_edit = gets.chomp.to_i
+  e_to_edit = Experiment.find(id_to_edit)
+  puts "D > Description edit"
+  puts "L > Location edit"
+  puts "S > Start date/time edit"
+  puts "E > End date/time edit"
+  choice = gets.chomp.upcase
+  case choice
+  when 'D'
+    puts "Enter new description:"
+    new_desc = gets.chomp
+    e_to_edit.update({:description => new_desc})
+    puts "Done. Press return to continue."
+    gets
+  when 'L'
+    puts "Enter new location:"
+    new_loc = gets.chomp
+    e_to_edit.update({:location => new_loc})
+    puts "Done. Press return to continue."
+    gets
+  when 'S'
+    puts "Enter new start date/time."
+    puts "Please use the format DD/MM/YYYY HH:MM:SS"
+    new_start = gets.chomp
+    e_to_edit.update({:start => new_start})
+    puts "Done. Press return to continue."
+    gets
+  when 'E'
+    puts "Enter new end date/time."
+    puts "Please use the format DD/MM/YYYY HH:MM:SS"
+    new_end = gets.chomp
+    e_to_edit.update({:end => new_end})
+    puts "Done. Press return to continue."
+    gets
+  else
+    puts 'Invalid input. Press return to continue.'
+    gets
+  end
+end
+
+def delete_experiment
+  list_experiments
+  puts "Please enter id number of experiment to delete."
+  puts "(If you enter an invalid id, nothing will be deleted)."
+  id_to_del = gets.chomp.to_i
+  e_to_destroy = Experiment.find(id_to_del)
+  e_to_destroy.destroy
+  puts "Done. Press return to continue."
+  gets
+end
+
+def list_experiments
+  puts
+  Experiment.all.each do |experiment|
+    puts "#{experiment.id}: #{experiment.description} at #{experiment.location}"
+    puts "Starting at #{experiment.start} and ending at #{experiment.end}."
+    puts "\n"
+  end
+end
+
 header
 main_menu
